140525d350d6accc3db8a338c475ce408b21d5b1
Gyubin <gbcompany27@gmail.com>
2023-05-09 19:46:49 +0900

    define the initial position of the banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,23 +1,47 @@
 import './Banner.css';
 
+import { useState } from 'react';
+
 export default function Banner(props) {
+    const [bannerIndex, setBannerIndex] = useState(1);
+    let viewportWidth = window.innerWidth;
+    const initialLeftPosition = -viewportWidth;
+
     let bannerNumber = 0;
     if (props.bannerList && Array.isArray(props.bannerList)) bannerNumber = props.bannerList.length;
 
-    props.bannerList.map((banner,index) => {
+    function BannerImageContainer (props) {
         return (
-            <div className='banner-image-container' key={'banner-'+String(index)}>
+            <a className='banner-image-container' href='/'>
                 <img
-                    src={banner.wideScreenImage}
+                    src={props.wideScreenImage}
                     className='banner-image wide'
                     alt='banner-wide-screen'></img>
                 <img
-                    src={banner.mobileImage}
+                    src={props.mobileImage}
                     className='banner-image mobile'
                     alt='banner-mobile'></img>
-            </div>
+            </a>
+        )
+    }
+    let BannerImageContainers = props.bannerList.map((banner,index) => {
+        return (
+            <BannerImageContainer
+                wideScreenImage={banner.wideScreenImage}
+                mobileImage={banner.mobileImage}
+                key={'banner-'+String(index)} />
         )
     })
+    const LastBanner = <BannerImageContainer  
+                            wideScreenImage={props.bannerList[props.bannerList.length-1].wideScreenImage}
+                            mobileImage={props.bannerList[props.bannerList.length-1].mobileImage}
+                            key='first-banner'/>
+    const FirstBanner = <BannerImageContainer  
+                            wideScreenImage={props.bannerList[1].wideScreenImage}
+                            mobileImage={props.bannerList[1].mobileImage}
+                            key='last-banner'/>
+    BannerImageContainers.push(FirstBanner);
+    BannerImageContainers.unshift(LastBanner);
 
     return (
         <div className='banner'>
@@ -25,8 +49,11 @@ export default function Banner(props) {
             <button className='banner-button right'></button>
             <div className='banner-counter'>
                 <span className='banner-counter-number'>
-                    {bannerNumber} / {bannerNumber}
+                    {bannerIndex} / {bannerNumber}
                 </span>
+            </div>
+            <div className='banner-contents' style={{'left':-initialLeftPosition}}>
+                {BannerImageContainers}
             </div>
         </div>
     )
