69ec1a8508f22c6aab41a91848f28063aa53cbd9
Gyubin <gbcompany27@gmail.com>
2023-05-10 23:52:48 +0900

    remove the test codes

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,7 +3,6 @@ import '../default.css';
 import Header from "../components/Header/Header"
 import Banner from '../components/Banner/Banner';
 import Footer from '../components/Footer/Footer';
-import Counter from '../components/Counter/Counter';
 
 export default function Main() {
     return (
@@ -11,7 +10,6 @@ export default function Main() {
             <Header/>
             <Banner/>
             <Footer/>
-            <Counter/>
         </div>
     )
 }
