a82633666faf82be9f5ccbaf017d2a6fcea0c755
Gyubin <gbcompany27@gmail.com>
2023-05-11 23:44:30 +0900

    add item

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,7 +3,6 @@ import '../default.css';
 import Header from "../components/Header/Header"
 import Banner from '../components/Banner/Banner';
 import Footer from '../components/Footer/Footer';
-import Item from '../components/Item/Item';
 
 export default function Main() {
     return (
@@ -11,7 +10,6 @@ export default function Main() {
             <Header/>
             <Banner/>
             <Footer/>
-            <Item></Item>
         </div>
     )
 }
