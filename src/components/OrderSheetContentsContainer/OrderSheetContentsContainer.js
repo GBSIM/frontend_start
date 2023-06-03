21362a3ae986ac0c10617764bd903c512a654023
gyubin <gbcompany27@gmail.com>
2023-06-03 12:07:28 +0900

    add total price to order sheet contents continaer

--- src/components/OrderSheetContentsContainer/OrderSheetContentsContainer.js
+++ src/components/OrderSheetContentsContainer/OrderSheetContentsContainer.js
@@ -1,11 +1,23 @@
+import './OrderSheetContentsContainer.css';
+
 import UnitContentsContainer from "../UnitContentsContainer/UnitContentsContainer";
 import OrderSheetBox from "../OrderSheetBox/OrderSheetBox";
 
 export default function OrderSheetContentsContainer(props) {
+    let totalPrice = 0;
+    props.items.map((item) => {
+        totalPrice = totalPrice + item.price * item.quantity;
+    })
+
     return (
-        <UnitContentsContainer title='주문서'>
+        <div className="order-sheet-contents-container">
+            <UnitContentsContainer title='주문서'>
             <OrderSheetBox items={props.items}/>
-        </UnitContentsContainer>
+            </UnitContentsContainer>
+            <div className='order-sheet-contents-container-row total-price'>
+                <h2 className='order-sheet-contents-text'>총 {totalPrice.toLocaleString()}원</h2>
+            </div>
+        </div>
     )
 }
 
