1cb0dcbb1d9576d56302541c9b37dda4315266ea
gyubin <gbcompany27@gmail.com>
2023-07-12 20:16:42 +0900

    implement calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -8,27 +8,64 @@ export default function Calendar(props) {
     const [selectedMonth, setMonth] = useState(today.getMonth() + 1);
     const [selectedDate, setDate] = useState(today.getDate());
 
+    const ChangeDate = (date) => {
+        setDate(date);
+    }
+
     const currentDate = new Date(selectedYear,selectedMonth-1,selectedDate);
-    let dateList = Array.from({length: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay() - 1}, () => 0);
+    let dateList = Array.from({length: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay()}, () => 0);
     for (var dateStep = 1; dateStep <= new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(); dateStep++) {
         dateList.push(dateStep);
     }
-    for (var dayStep = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDay() - 1);dayStep < 6;dayStep++) {
+    console.log((new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDay()));
+    for (var dayStep = (new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDay());dayStep < 6;dayStep++) {
         dateList.push(0);
     }
 
-    const CalendarDays = dateList.map((date) => {
+    const CalendarDays = dateList.map((date,index) => {
         return (
-            <CalendarDay date={date} isOn={date===selectedDate}/>
+            <CalendarDay 
+                date={date} 
+                isOn={date===selectedDate}
+                clickEvent={ChangeDate}
+                key={'calendar-day-'+index}/>
         )
     })
+
+    const moveToPreviousMonth = () => {
+        if (selectedMonth === 1) {
+            setMonth(12);
+            setYear(selectedYear-1);
+        } else {
+            setMonth(selectedMonth - 1);
+        }
+    }
+
+    const moveToNextMonth = () => {
+        if (selectedMonth === 12) {
+            setMonth(1);
+            setYear(selectedYear + 1);
+        } else {
+            setMonth(selectedMonth + 1);
+        }
+    }
 
     return (
         <div className='calendar'>
             <div className='calendar-row'>
+                <button className='calendar-month-arrow-button' onClick={() => moveToPreviousMonth()}>
+                    <img className='calendar-month-arrow-button-image' 
+                         alt='left-arrow'
+                         src={require('../../icons/inequity_left_white.png')}/>
+                </button>
                 <h2 className='calendar-month'>
                     {selectedMonth}월
                 </h2>
+                <button className='calendar-month-arrow-button' onClick={() => moveToNextMonth()}>
+                    <img className='calendar-month-arrow-button-image' 
+                         alt='right-arrow'
+                         src={require('../../icons/inequity_right_white.png')}/>
+                </button>
             </div>
             <div style={{'minHeight':'20px'}}></div>
             <div className='calendar-days-container'>
@@ -53,7 +90,7 @@ function CalendarDay(props) {
         } else {
             return (
                 <div className='calendar-day'>
-                    <button className='calendar-day-button'>
+                    <button className='calendar-day-button' onClick={() => props.clickEvent(props.date)}>
                         <span className='calendar-day-text'>
                             {props.date}
                         </span>
