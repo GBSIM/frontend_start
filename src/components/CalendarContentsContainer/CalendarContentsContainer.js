34a213dca509d494e538220a919bee01cb11fcd9
gyubin <gbcompany27@gmail.com>
2023-08-12 14:22:36 +0900

    add calendar contents container

---
+++ src/components/CalendarContentsContainer/CalendarContentsContainer.js
@@ -0,0 +1,56 @@
+import UnitContentsContainer from "../UnitContentsContainer/UnitContentsContainer";
+import './CalendarContentsContainer.css';
+
+import Calendar from "../Calendar/Calendar";
+
+export default function CalendarContetnsContainer(props) {
+    const DateGuide = 
+        <div className="calendar-contents-container-date-guide">
+            <h3 className="calendar-contents-container-date">
+                {props.year}년 {props.month}월 {props.day}일을 선택하셨어요.
+            </h3>
+        </div>
+
+    let TimeGuide;
+    if (props.isTimeNeeded) {
+        if (props.minute > 0) {
+            TimeGuide = 
+                <div className="calendar-contents-container-time-guide">
+                    <h3 className="calendar-contents-container-time">
+                        {props.hour}시에 {props.minute}분에 방문해주세요.
+                    </h3>
+                </div>
+        } else {
+            TimeGuide = 
+                <div className="calendar-contents-container-time-guide">
+                    <h3 className="calendar-contents-container-time">
+                        {props.hour}시에 매장으로 방문해주세요.
+                    </h3>
+                </div>
+        }   
+    }
+    
+
+    return (
+        <UnitContentsContainer title={props.title}>
+            <div style={{'minHeight':'30px'}}></div>
+            <Calendar/>
+            <div style={{'minHeight':'20px'}}></div>
+            {DateGuide}
+            <div style={{'minHeight':'5px'}}></div>
+            {TimeGuide}
+            <div style={{'minHeight':'30px'}}></div>
+            <div style={{'width':'100%','minHeight':'1px','backgroundColor':'#eee'}}></div>
+        </UnitContentsContainer>
+    )
+}
+
+CalendarContetnsContainer.defaultProps = {
+    title: '픽업날짜',
+    year: 2023,
+    month: 8,
+    day: 15,
+    hour: 15,
+    minute: 0,
+    isTimeNeeded: true,
+}
--- src/views/OrderView.js
+++ src/views/OrderView.js
@@ -3,34 +3,22 @@ import '../default.css';
 import Header from "../components/Header/Header";
 import OrderSheetContentsContainer from '../components/OrderSheetContentsContainer/OrderSheetContentsContainer';
 import CustomerInfoContentsContainer from '../components/CustomerInfoContentsContainer/CustomerInfoContentsContainer';
-import Calendar from '../components/Calendar/Calendar';
 import ShippingContentsContainer from '../components/ShippingContentsContainer/ShippingContentsContainer';
+import CalendarContetnsContainer from '../components/CalendarContentsContainer/CalendarContentsContainer';
+import OrderButton from '../components/OrderButton/OrderButton';
 
 export default function OrderView(props) {
     let CalendarBox;
     if (props.status === '픽업주문') {
         CalendarBox =
-            <div style={{'width': '100%','display':'flex','flexDirection':'column','alignItems':'center'}}>
-                <div style={{'width': '100%','display':'flex','flexDirection':'row','alignItems':'center','justifyContent':'center'}}>
-                    <h1 style={{'color':'#bbb'}}>
-                        픽업날짜
-                    </h1>
-                </div>
-                <div style={{'minHeight':'30px'}}></div>
-                <Calendar/>
-                <div style={{'minHeight':'30px'}}></div>
-                <div style={{'width': '100%','display':'flex','flexDirection':'row','alignItems':'center','justifyContent':'center'}}>
-                    <span style={{'color':'#666'}}>
-                        2023년 7월 31일을 선택하셨어요.
-                    </span>
-                </div>
-                <div style={{'minHeight':'5px'}}></div>
-                <div style={{'width': '100%','display':'flex','flexDirection':'row','alignItems':'center','justifyContent':'center'}}>
-                    <span style={{'color':'#666'}}>
-                        매장에 14:00에 방문해주세요.
-                    </span>
-                </div>
-            </div>
+            <CalendarContetnsContainer
+                title='픽업날짜'
+                isTimeNeeded={true}/>
+    } else if (props.status === '선물주문') {
+        CalendarBox =
+            <CalendarContetnsContainer
+                title='퀵 배송 날짜'
+                isTimeNeeded={false}/>
     }
 
     return (
@@ -46,6 +34,8 @@ export default function OrderView(props) {
                     <ShippingContentsContainer/>
                     <div style={{'minHeight':'30px'}}></div>
                     {CalendarBox}
+                    <div style={{'minHeight':'30px'}}></div>
+                    <OrderButton/>
                 </div>
             </div>
         </div>
