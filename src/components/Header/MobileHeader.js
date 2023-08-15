b3f4be2721ef490cd1085c4362ac0822ffbcd44a
Gyubin <gbcompany27@gmail.com>
2023-08-15 11:46:17 +0900

    add display condition to likes and cart button in mobile header

--- src/components/Header/MobileHeader.js
+++ src/components/Header/MobileHeader.js
@@ -5,12 +5,19 @@ import LikesPageButton from '../LikesPageButton/LikesPageButton';
 import CartPageButton from '../CartPageButton/CartPageButton';
 
 export default function MobileHeader(props) {
+    let LikeButton;
+    let CartButton;
+    if (props.isLogined) {
+        LikeButton = <LikesPageButton/>
+        CartButton= <CartPageButton/>
+    }
+
     return (
         <div className="mobile-header" style={{"minHeight":props.height}}>
             <LogoButton height='40px' width='100px'></LogoButton>
             <div className='mobile-header-spacer'></div>
-            <LikesPageButton/>
-            <CartPageButton/>
+            {LikeButton}
+            {CartButton}
         </div>
     )
 }
