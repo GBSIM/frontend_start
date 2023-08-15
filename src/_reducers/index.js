f1c0c292975b26209d25f897bc33711fd2dfcacc
Gyubin <gbcompany27@gmail.com>
2023-08-15 10:26:12 +0900

    add item to reducers

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 
 import nav from "./nav";
 import user from "./user";
+import item from "./item";
 
 const rootReducer = combineReducers({
-    nav, user
+    nav, user, item
 });
 
 export default rootReducer;
---
+++ src/_reducers/item.js
@@ -0,0 +1,53 @@
+export const SAVE_ITEM_INFO = "ITEM/SAVE_ITEM_INFO";
+
+export const saveItemInfo = (info) => ({type:SAVE_ITEM_INFO, info:info});
+
+const initialState = {
+    name: '제품명',
+    price: 3000,
+    tag: '인기',
+    menuClass: '',
+    stock: 100,
+    orderType: 'delivery',
+    quantity: 1,
+    option: {},
+    isChecked: true,
+    imageUrl: '',
+    detailImageUrl: [],
+    intro: '',
+    options: [],
+    minimumOrderQuantity: 1,
+    gradients: [],
+    detail: '',
+    purchaseable: true,
+}
+
+const item = (state = initialState, action) => {
+    switch (action.type) {
+        case SAVE_ITEM_INFO:
+            return {
+                ...state,
+                name: action.info.name,
+                price: action.info.price,
+                tag: action.info.tag,
+                menuClass: action.info.menuClass,
+                stock: action.info.stock,
+                orderType: action.info.orderType,
+                quantity: action.info.quantity,
+                option: action.info.option,
+                isChecked: action.info.isChecked,
+                imageUrl: action.info.imageUrl,
+                detailImageUrl: action.info.detailImageUrl,
+                intro: action.info.intro,
+                options: action.info.options,
+                minimumOrderQuantity: action.info.minimumOrderQuantity,
+                gradients: action.info.gradients,
+                detail: action.info.detail,
+                purchaseable: action.info.purchaseable,
+            }
+        default:
+            return state;
+    }
+};
+
+export default item;
